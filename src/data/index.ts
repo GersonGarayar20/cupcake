7bc154e6316f3175d06dbe95d77fb06baa316743
Gerson Garayar <garayargerson20@gmail.com>
2023-11-05 18:19:26 -0500

    feat: base

---
+++ src/data/index.ts
@@ -0,0 +1,32 @@
+export const productos = [
+  {
+    nombre: "Pastel de Chocolate",
+    descripcion: "Un pastel rico y decadente de chocolate negro, perfecto para los amantes del chocolate.",
+    precio: 20.99,
+    image: "pastel_chocolate.webp"
+  },
+  {
+    nombre: "Cupcakes de Fresa",
+    descripcion: "Cupcakes esponjosos con crema de fresa y decoración de frutas frescas.",
+    precio: 2.99,
+    image: "cupcakes-fresa.webp"
+  },
+  {
+    nombre: "Galletas de Avena y Pasas",
+    descripcion: "Galletas caseras de avena con pasas y un toque de canela.",
+    precio: 1.49,
+    image: "galletas-de-avena-con-pasas.jpg"
+  },
+  {
+    nombre: "Tarta de Manzana",
+    descripcion: "Una tarta clásica de manzana con un crujiente de canela y azúcar.",
+    precio: 18.99,
+    image: "tarta-manzan-arguinano-t.jpg.webp"
+  },
+  {
+    nombre: "Éclair de Vainilla",
+    descripcion: "Éclairs rellenos de crema de vainilla y cubiertos con glaseado de chocolate.",
+    precio: 3.49,
+    image: "éclair-de-vainilla.jpg"
+  }
+]
